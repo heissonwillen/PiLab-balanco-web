5d5c4d0bb84ee216476480e6a73eb479d9ed586a
heissonwillen <heisson70@gmail.com>
2021-10-08 16:36:50 -0300

    add recursive listing

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const fs = require('fs')
+const parse = require('csv-parse')
 
-app.use(express.json());
+const app = express()
 
-const balances = [
-    { name: "ativo1" },
-    { name: "ativo2" },
-    { name: "ativo3" },
-];
+app.use(express.json())
+
+const nestBalance = (arr, id, children) => {
+    if (id === '0' && arr.length == 0) {
+        arr.push(children[0])
+    } else {
+        arr.forEach(balance => {
+            if (balance.id === id) {
+                balance.children = [...(balance.children || []), ...children]
+            } else {
+                nestBalance(balance.children || [], id, children)
+            }
+        })
+    }
+}
 
 app.get("/balance", (req, res) => {
-    res.send(balances);
-});
+    balanceList = []
+    nestedBalances = []
+    fs.createReadStream('categories.csv')
+        .pipe(parse({ delimiter: '\t' }))
+        .on('data', balRow => {
+            balanceList.push(balRow)
+        }).on('end', () => {
+            fs.createReadStream('releases.csv')
+                .pipe(parse({ delimiter: '\t' }))
+                .on('data', rlsRow => {
+                    balanceList.forEach(balRow => {
+                        balRow[3] = parseInt(balRow[3])
+                        if (balRow[0] == rlsRow[3]) {
+                            balRow[3] += parseInt(rlsRow[1]) * parseInt(rlsRow[2])
+                        }
+                    })
+                }).on('end', () => {
+                    balanceList.forEach(row => {
+                        const [id, name, father_id, initial_value] = row
+                        nestBalance(nestedBalances, father_id, [{ id: id, name: name, value: initial_value }])
+                    })
+                    res.send(nestedBalances)
+                })
+        })
+})
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
